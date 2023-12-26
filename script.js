24956cf0349d5b60efb6f7d2ddb7678ac4231a49
j0shyang <j0shyang@proton.me>
2023-12-25 18:37:51 -0800

    Rock Paper Scissors functionality completed

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
 function getComputerChoice() {
-    return Math.floor((Math.random() * 3) + 1);
-    // console.log(Math.floor((Math.random() * 3) + 1));
+    let choice = Math.floor((Math.random() * 3) + 1);
+
+    if (choice === 1) {
+        return "rock";
+    } else if (choice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1);
+    computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.toLowerCase().slice(1);
+
+    console.log(`You chose: ${playerSelection}`);
+    console.log(`Computer chose: ${computerSelection}`)
+
+    switch (playerSelection + computerSelection) {
+        // same choice
+        case "RockRock":
+        case "PaperPaper":
+        case "ScissorsScissors":
+            return "It's a tie!"
+            break;
+        // player wins
+        case "RockScissors":
+        case "PaperRock":
+        case "ScissorsPaper":
+            return `You won! ${playerSelection} beats ${computerSelection}!`
+            break;
+        // computer wins
+        case "RockPaper":
+        case "PaperScissors":
+        case "ScissorsRock":
+            return `You lose! ${computerSelection} beats ${playerSelection}!`
+        default:
+            return "Your choice does not exist!";
+    }
+}
+
+const playerSelection = "ROCK";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection.toLowerCase(), computerSelection));
